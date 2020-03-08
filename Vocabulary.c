42a4f69045bb2ac89aa69d728ddc273d9022b648
notherland <55365450+notherland@users.noreply.github.com>
2020-03-08 15:11:41 +0400

    Update Vocabulary.c

--- Vocabulary.c
+++ Vocabulary.c
@@ -1,59 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include "hash_map.c"
+#include <assert.h>
 
-const char MAXWORDLEN  = 20;
+const char MAXWORDLEN  = 250;
 const char r = 3;
 const char p = 10;
 
+//возвращает длину текствого файла
+int Text_Len (FILE* file)
+{
+    fseek(file, 0 , SEEK_END);
+    int length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return length;
+}
+
 int Hash (char* str){
     int i = 0, hash = 0, R = 1;
-    while (isalpha(str[i]))
+
+    while (str[i] != '\0')
     {
         hash = hash + (str[i] * R);
         R= R*r;
+        i += 1;
     }
+
     return hash % p;
 }
 
-char* Read_Word(){
-    char *str = (struct word*)calloc(1, sizeof(struct word));
-    int len = 0;
-    scanf ("%c", str[len]);
-    while (str[len] != " ")
-    {
-        len += 1;
-        scanf ("%c", str[len]);
-    }
-    if (len == 0)
-        return -1;
-    else return str;
-}
+//считывает название, список слов, распределяет в хэш-таблице
+char *Read_Input(struct word* top, int *count, FILE *file){
+    int textlen = Text_Len(file);
 
-struct word *Read_Words(){
-    struct word* top = CreateTable();
-    char *str = (char*)calloc (MAXWORDLEN, sizeof(char));
+    char *str = (char*) calloc(textlen + 1, sizeof(char));
+    fread (str, sizeof(char), textlen, file);
+
+    char *fname = strtok (str, " ");
+
     int key = 0;
-    while (Read_Word() != -1)
+    while (str = strtok(NULL, " "))
     {
+        printf ("word: %s\n", str);
         key = Hash (str);
-        AddTable (top, key, str);
+        printf ("key:  %d\n", key);
+        Add_Table (top, key, str, *count);
+        *count += 1;
     }
-    return top;
+
+    free (str);
+    return fname;
 }
 
-Find_Words ()
+//ищет слово в хэш-таблице
+void Find_word(char *str, struct word* list, char* string)
+{
+    int key = Hash(str);
+    struct word *ptr = list[key].ptr;
 
-Print(){
+    while (ptr != NULL)
+    {
+        if (strcmp(ptr->str, str) == 0)
+        {
+            string[ptr->place] +=1;
+            return;
+        }
+        ptr = ptr->ptr;
+    }
+
+    return;
+}
+
+void Print (char *string, int count){
+    int i = 0;
+    for (i = 0; i < count; i += 1)
+        printf ("%d ", string[i]);
+    return;
+}
+
+void Count_Words (char *fname, struct word* list, int count)
+{
+    FILE *file = fopen(fname, "r");
+    int textlen = Text_Len(file);
+
+    char *str = calloc(textlen + 1, sizeof(char));
+    fread (str, sizeof(char), textlen, file);
+
+    char *array = calloc (count, sizeof(char));
+
+    char *ptr = strtok (str, " /,.!?;:’'\n\0");
+    while (ptr != NULL)
+    {
+        //printf ("%s\n", ptr);
+        Find_word (ptr, list, array);
+        ptr = strtok (NULL, " /,.!?;:’'\n\0");
+    }
+
+    Print(array, count);
+
+    free (str);
+    free (array);
+    return;
 }
 
 int main() {
-    struct word* list = (struct table*) calloc (1, sizeof(struct word));
-    char *fname = Read_Word();
-    scanf ("%s", fname);
-    FILE *f = fopen(fname, "r");
-    list = Read_Words ()
-    Count_Words (f);
+    struct word* list = Create_Table();
+    FILE * file = fopen("C:\\Users\\nothe\\CLionProjects\\untitled7\\test.txt", "r");
+    assert(file);
+    int count = 0;
+    char *fname = Read_Input (list, &count, file);
+    Count_Words (fname, list, count);
+    Destroy_Table(list);
 }
 
